abcefcc5ad90ab4f8eb2e947ae853cdc967271ae
rivan Dilmaev <xl92@mail.ru>
2025-06-23 21:06:57 +0300

    add: httprouter

--- cmd/todolist-v1/main.go
+++ cmd/todolist-v1/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	router := httprouter.New()
+	router.GET("/", Home)
+
+	start(router)
+}
+
+func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Write([]byte("Home"))
+}
+
+func start(router *httprouter.Router) {
+	const timeout = 15 * time.Second
+
+	server := &http.Server{
+		Addr:         "8080",
+		Handler:      router,
+		WriteTimeout: timeout,
+		ReadTimeout:  timeout,
+		IdleTimeout:  timeout,
+	}
+
+	fmt.Printf("Сервер запущен на 8080")
+	log.Fatal(server.ListenAndServe())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Igrok95Ronin/todolist-v1.git
 
 go 1.24.0
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
